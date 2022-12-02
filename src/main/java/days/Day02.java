07f451f4ae76160e04c99316d9af3ecf53333b25
BrettBauereisCallibrity <brett.bauereis@callibrity.com>
2022-12-02 18:36:54 -0500

    Day 2 more

--- src/main/java/days/Day02.java
+++ src/main/java/days/Day02.java
@@ -11,13 +11,20 @@ public class Day02 {
         System.out.println("Day 2 - Part 2: " + part2(filePathBase));
     }
 
+    private static char rock = '1';
+    private static char paper = '2';
+    private static char scissors = '3';
+    private static char win = '6';
+    private static char tie = '3';
+    private static char loss = '0';
+
     public static Integer part1(String filePathBase)
     {
         ArrayList<String> lines = Input.readAsLines(filePathBase, "Day02.txt");
         ArrayList<char[]> scoreTemplates = new ArrayList<>();
-        scoreTemplates.add(new char[]{'X','1', '3', '0', '6'});
-        scoreTemplates.add(new char[]{'Y','2', '6', '3', '0'});
-        scoreTemplates.add(new char[]{'Z','3', '0', '6', '3'});
+        scoreTemplates.add(new char[]{'X', rock, tie, loss, win});
+        scoreTemplates.add(new char[]{'Y', paper, win, tie, loss});
+        scoreTemplates.add(new char[]{'Z', scissors, loss, win, tie});
 
         return getTheScore(lines, scoreTemplates);
     }
@@ -26,9 +33,9 @@ public class Day02 {
     {
         ArrayList<String> lines = Input.readAsLines(filePathBase, "Day02.txt");
         ArrayList<char[]> scoreTemplates = new ArrayList<>();
-        scoreTemplates.add(new char[]{'Z','6', '2', '3', '1'});
-        scoreTemplates.add(new char[]{'Y','3', '1', '2', '3'});
-        scoreTemplates.add(new char[]{'X','0', '3', '1', '2'});
+        scoreTemplates.add(new char[]{'X', loss, scissors, rock, paper});
+        scoreTemplates.add(new char[]{'Y', tie, rock, paper, scissors});
+        scoreTemplates.add(new char[]{'Z', win, paper, scissors, rock});
 
         return getTheScore(lines, scoreTemplates);
     }
@@ -37,34 +44,29 @@ public class Day02 {
     {
         ArrayList<String> newLines = new ArrayList<>();
         for (String line : lines) {
-            newLines.add(convertValues(line, scoreTemplates));
+            newLines.add(convertLettersToScores(line, scoreTemplates));
         }
 
-        ArrayList<Integer> points = new ArrayList<>();
+        ArrayList<Integer> scores = new ArrayList<>();
         for (String line : newLines) {
             String[] splits = line.split(" ");
-            points.add(Integer.parseInt(splits[0]));
-            points.add(Integer.parseInt(splits[1]));
+            scores.add(Integer.parseInt(splits[0]));
+            scores.add(Integer.parseInt(splits[1]));
         }
 
-        return points.stream().mapToInt(Integer::intValue).sum();
+        return scores.stream().mapToInt(Integer::intValue).sum();
     }
 
-    public static String convertValues(String line, ArrayList<char[]> scoreTemplates)
+    public static String convertLettersToScores(String line, ArrayList<char[]> scoreTemplates)
     {
         for (char[] template : scoreTemplates)
         {
             if (line.contains(String.valueOf(template[0])))
             {
-                line = lineReplace(line, template);
+                return line.replace(template[0], template[1]).replace('A', template[2]).replace('B', template[3]).replace('C', template[4]);
             }
         }
-        return line;
-    }
 
-    public static String lineReplace(String line, char[] chars)
-    {
-        line = line.replace(chars[0], chars[1]).replace('A', chars[2]).replace('B', chars[3]).replace('C', chars[4]);
         return line;
     }
 
