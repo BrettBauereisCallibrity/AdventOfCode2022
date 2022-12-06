571311aa1df123ef1f4d0f0be123857872bd3927
BrettBauereisCallibrity <brett.bauereis@callibrity.com>
2022-12-06 13:56:14 -0500

    Day 6 cleanup

--- src/main/java/days/Day06.java
+++ src/main/java/days/Day06.java
@@ -5,9 +5,10 @@ import main.java.Utils;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Day06 {
-
+    
     public static void showMeTheMoney(String filePathBase) {
         System.out.println("Day 6 - Part 1: " + part1(filePathBase));
         System.out.println("Day 6 - Part 2: " + part2(filePathBase));
@@ -15,30 +16,25 @@ public class Day06 {
 
     public static Integer part1(String filePathBase)
     {
-        return findUniqueCharsStringLengthX(4, filePathBase);
+        return findPositionOfUniqueCharStringOfLengthX(4, filePathBase);
     }
 
     public static Integer part2(String filePathBase)
     {
-        return findUniqueCharsStringLengthX(14, filePathBase);
+        return findPositionOfUniqueCharStringOfLengthX(14, filePathBase);
     }
 
-    private static Integer findUniqueCharsStringLengthX(Integer lengthOfUniqueString, String filePathBase)
+    private static Integer findPositionOfUniqueCharStringOfLengthX(Integer lengthOfUniqueString, String filePathBase)
     {
         ArrayList<String> lines = Input.readAsLines(filePathBase, "Day06.txt");
-        ArrayList<Character> fileAsChars = Utils.convertStringToArraylistOfCharacters(lines.get(0));
+        ArrayList<Character> allCharsFromFile = Utils.convertStringToArraylistOfCharacters(lines.get(0));
 
-        for (int i = lengthOfUniqueString - 1; i < fileAsChars.size(); i++)
+        for (int i = lengthOfUniqueString; i < allCharsFromFile.size(); i++)
         {
-            ArrayList<Character> xInARow = new ArrayList<>();
-            for (int n = 0; n <= lengthOfUniqueString - 1; n++){
-                xInARow.add(fileAsChars.get(i-n));
-            }
-
-            HashSet<Character> hashed = new HashSet<>(xInARow);
-            if (hashed.size() == lengthOfUniqueString)
+            List<Character> xInARow = allCharsFromFile.subList(i-lengthOfUniqueString, i);
+            if (xInARow.size() == new HashSet<>(xInARow).size())
             {
-                return i+1;
+                return i;
             }
         }
         return 0;
